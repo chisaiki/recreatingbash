3081c2548d9cba958c39c4c045229a96c2777e96
mishi <mishitarahman@gmail.com>
2023-10-28 18:53:41 -0400

    updated upper.c

--- upper.c
+++ upper.c
@@ -5,4 +5,11 @@
 int main(int argc, char *argv[])
 {
     
+    for(int i = 0; i < argc; i++)
+    {   
+        for(int j = 0; argv[i][j] != '\0'; j++)
+        {
+            
+        }
+    }
 }
