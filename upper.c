276df4498755e8e08313793f93e7902195ea432b
mishi <mishitarahman@gmail.com>
2023-10-28 19:10:58 -0400

    added upper function

--- upper.c
+++ upper.c
@@ -2,16 +2,20 @@
 
 //Make all arguments on the command into uppercase, it will numbers 
 
-int main(int argc, char *argv[])
+void printUpper(int a, char *b[])
 {
-    
-    for(int i = 1; i < argc; i++)
+    for(int i = 1; i < a; i++)
     {   
-        for(int j = 0; argv[i][j] != '\0'; j++)
+        for(int j = 0; b[i][j] != '\0'; j++)
         {
-            printf("%c", toupper(argv[i][j]));
+            printf("%c", toupper(b[i][j]));
         }
 
         printf(" ");
     }
+}
+
+int main(int argc, char *argv[])
+{
+    printUpper(argc, argv);
 } 
