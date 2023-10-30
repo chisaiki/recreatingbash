719eca845a0556b6d75889e9b709bb39355fec81
mishi <mishitarahman@gmail.com>
2023-10-30 12:53:09 -0400

    updated diffdate

--- diffdate/diffdate.c
+++ diffdate/diffdate.c
@@ -5,9 +5,13 @@ int main(int argc, char *argv[])
     switch(argc)
     {
         case '1':
+        fprintf(stderr, "Error: Not Enough Arguments.");
         break;
 
         case '2':
+        break;
+
+        case '3': 
         break;
     }
 }
