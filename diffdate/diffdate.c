03424383049a26f7ce4ff250213d21dc9b3e8d90
mishi <mishitarahman@gmail.com>
2023-10-30 12:56:31 -0400

    updated diffdate

--- diffdate/diffdate.c
+++ diffdate/diffdate.c
@@ -3,15 +3,15 @@
 int main(int argc, char *argv[])
 {
     switch(argc)
-    {
-        case '1':
+    { 
+        case 1: //Error: I initially typed case '1', this results in an error because argc is an int not char!
         fprintf(stderr, "Error: Not Enough Arguments.");
         break;
 
-        case '2':
+        case 2:
         break;
 
-        case '3': 
+        case 3: 
         break;
     }
 }
